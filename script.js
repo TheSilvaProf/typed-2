80d6b19d3c6cc768f6fce9df887c3f54cc1bc37c
TheSilvaProf <fabio01@protonmail.ch>
2023-03-12 08:39:36 -0300

    Redondo

---
+++ script.js
@@ -0,0 +1,8 @@
+var typed = new Typed("#typed", {
+    strings: ['Hello there!', 'How are you?'],
+    typeSpeed: 35,
+    startDelay: 500,
+    backDelay: 1000,
+    backSpeed: 35,
+    loop: true
+});
